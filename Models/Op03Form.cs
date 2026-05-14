b4e72396c7f28ca6a8990abf1707ddc26d590695
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-05-14 16:39:59 +0900

    #3

--- Models/Op03Form.cs
+++ Models/Op03Form.cs
@@ -5,17 +5,17 @@ namespace WebApp_Exercise.Models;
 public class Op03Form
 {
     [Display(Name = "値1")]
-    [Required(ErrorMessage = "{0}は入力必須です。")]
+    [Required(ErrorMessage = "{0}を入力してください。")]
     [Range(0, 1000, ErrorMessage = "{0}は{1}以上{2}以下の範囲で入力してください。")]
     public int? Value1 { get; set; }
 
     [Display(Name = "値2")]
-    [Required(ErrorMessage = "{0}は入力必須です。")]
+    [Required(ErrorMessage = "{0}を入力してください。")]
     [Range(0, 1000, ErrorMessage = "{0}は{1}以上{2}以下の範囲で入力してください。")]
     public int? Value2 { get; set; }
 
     [Display(Name = "計算の種類")]
-    [Required(ErrorMessage = "{0}は入力必須です。")]
+    [Required(ErrorMessage = "{0}を選択してください。")]
     [Range(1, 5, ErrorMessage = "計算の種類が選択されていません")]
     public int? Opt { get; set; }
 
