00b8db812a2b8fd9d8bfa966a639382f1105f395
mizuto26 <192568839+mizuto26@users.noreply.github.com>
2026-05-15 15:40:47 +0900

    #11

--- Models/Exercise07Form.cs
+++ Models/Exercise07Form.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 namespace WebApp_Exercise.Models;
 
 public class Exercise07Form
 {
+    [Display(Name = "値1")]
+    [Required(ErrorMessage = "{0}は入力必須です。")]
+    [Range(0, 100, ErrorMessage = "{0}は{1}～{2}までの数字で入力してください。")]
     public int Value1 { get; set; } = 0;
+
+    [Display(Name = "値2")]
+    [Required(ErrorMessage = "{0}は入力必須です。")]
+    [Range(0, 100, ErrorMessage = "{0}は{1}～{2}までの数字で入力してください。")]
     public int Value2 { get; set; } = 0;
+
     public int Answer { get; set; } = 0;
 }
